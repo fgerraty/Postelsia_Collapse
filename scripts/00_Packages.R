88c364c6542efbf256b902bb9133eaa79dc01c7b
Frankie Gerraty <frankiegerraty@gmail.com>
2024-12-23 15:12:13 -0800

    Mapping functions

--- scripts/00_Packages.R
+++ scripts/00_Packages.R
@@ -8,6 +8,6 @@
 # Part 1: Load Packages --------------------------------------------------
 
 # Load packages
-packages <- c("tidyverse", "mgcv", "ggthemes")
+packages <- c("tidyverse", "mgcv", "ggthemes", "maps", "mapdata", "cowplot", "ggpubr", "sf")
 
 pacman::p_load(packages, character.only = TRUE); rm(packages)
