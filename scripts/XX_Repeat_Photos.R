f41627167c822e9b324d1dbdb2f42050da132a73
Frankie Gerraty <frankiegerraty@gmail.com>
2025-01-15 16:19:23 -0800

    Working with repeat photos dataset

--- scripts/XX_Repeat_Photos.R
+++ scripts/XX_Repeat_Photos.R
@@ -5,7 +5,18 @@
 # Script XX: Repeat Photo Analyses #######################################
 #-------------------------------------------------------------------------
 
-panoramic_photos <- read_csv("data/processed/panoramic_photos.csv")
+panoramic_photos <- read_csv("data/processed/panoramic_photos.csv") %>% 
+  mutate(period = case_when(year < 2014 ~ "pre_MHW", 
+                            year >= 2014 & year <= 2016 ~ "MHW",
+                            year > 2016 ~ "post_MHW"),
+         period = factor(period, levels = c("pre_MHW", "MHW", "post_MHW"))) %>% 
+  group_by(site_id, pan) %>% 
+  mutate(percent_of_max_postelsia = (percent_postelsia /  max(percent_postelsia, na.rm = TRUE)) * 100,
+         percent_of_max_mussel = (percent_mussel / max(percent_mussel))*100) 
+  
+
+
+
 
 panoramic_photos_long <- panoramic_photos %>% 
   mutate(photo_id = paste(site_id, pan, sep = "_")) %>% 
@@ -47,7 +58,45 @@ photo_summary <- ggplot(panoramic_photos_long, aes(x=year, y=percent_of_max,
         legend.title=element_text(face="bold"),
         legend.position = "inside", 
         legend.position.inside = c(0.8, 0.5)) 
+photo_summary
 
 ggsave("output/extra_figures/photo_summary.png", photo_summary, 
        width = 6, height = 6, units = "in", dpi = 600)
 
+
+
+##################################
+#Postelsia vs Mussels Plot #######
+##################################
+
+mean_plot_df <- panoramic_photos %>% 
+  group_by(period) %>% 
+  summarise(mean_postelsia = mean(percent_postelsia),
+            mean_mussel = mean(percent_mussel))
+
+
+ggplot(panoramic_photos, aes(x=percent_mussel, y=percent_postelsia, 
+                                                   color = period)) +
+  #Add points
+  geom_point(size = 3, alpha = .5)+
+  geom_point(data = mean_plot_df, 
+             aes(x=mean_mussel, y=mean_postelsia), 
+             size = 10)+
+  scale_color_manual(values = c("#648fff", "#dc267f", "#ffb000"),
+                     labels = c("Pre-MHW", "MHW", "Post-MHW"))+
+  labs(x="Mussel abundance (percent cover)",
+       y = "Postelsia abundance (percent cover)",
+       color = "Period")+
+  theme_few()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1.1,hjust = 1),
+        panel.border = element_rect(linewidth = 2),
+        strip.text = element_text(face = "bold"),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold"),
+        legend.title=element_text(face="bold"),
+        legend.position = "inside", 
+        legend.position.inside = c(0.8, 0.75)) 
+
+
+
+  
--- scripts/XX_Stars.R
+++ scripts/XX_Stars.R
@@ -24,7 +24,7 @@ stars_summary <- stars %>%
 ggplot(stars_summary, aes(x=year, y=sum_pisaster_biomass_g))+
   annotate("rect", xmin = 2014, xmax = 2016, ymin = -Inf, ymax = Inf, 
            fill = "red", alpha = 0.2) +
-  geom_point()+
+  geom_point(color = "purple3")+
   facet_wrap(facets = "site_id",
              scales = "free_y"
            )+
@@ -54,3 +54,19 @@ ggplot(stars_summary, aes(x=year, y=percent_of_max))+
         axis.title.x = element_text(face = "bold"),
         axis.title.y = element_text(face = "bold")) 
 
+
+# Part X: 2012-2021 Summary Plot ----------------------------------------------
+ggplot(stars_summary, aes(x=year, y=percent_of_max))+
+  annotate("rect", xmin = 2014, xmax = 2016, ymin = -Inf, ymax = Inf, 
+           fill = "red", alpha = 0.2) +
+  geom_point(size = 3, alpha = .5, color = "purple3")+
+  geom_smooth(color = "purple3", fill = "purple1")+
+  coord_cartesian(xlim = c(2012, 2021))+
+  labs(y = "P. ochraceous biomass (% of maximum)",
+       x = "Survey year")+
+  theme_few()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1.1,hjust = 1),
+        panel.border = element_rect(linewidth = 2),
+        strip.text = element_text(face = "bold"),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold")) 
