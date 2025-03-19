00811929a6d2772457c18b017408740f77026f9c
Frankie Gerraty <frankiegerraty@gmail.com>
2025-03-19 14:38:31 -0700

    Marine heatwave visualization

--- scripts/04_Heatwave.R
+++ scripts/04_Heatwave.R
@@ -48,7 +48,7 @@ clim_site <- lapply(names(SST_site), function(site) {
 names(clim_site) <- names(SST_site)  # Assign names to the list
 
 
-# Part 2C: Detect MHW events for all sites ####
+# Part 2C: Detect and categorize MHW events for all sites ####
 
 # Detect MHWs from each site's climatology data
 events <- lapply(names(clim_site), function(site) {
@@ -57,10 +57,17 @@ events <- lapply(names(clim_site), function(site) {
 names(events) <- names(clim_site)  # Assign names to the list
 
 
+# Categorize MHW events for all sites
+MHW_categories <- lapply(names(events), function(site) {
+  category(events[[site]], S = TRUE, name = site)
+})
+names(MHW_categories) <- names(events)  # Assign names to the list
+
+
 # Part 2D. Summarize marine heatwave (MHW) days per year for each site ####
 
 # Extract and summarize MHW days from the "climatology" sub-list
-mhw_days_per_site <- lapply(names(events), function(site) {
+MHW_days_per_site <- lapply(names(events), function(site) {
   events[[site]]$climatology %>%
     filter(event) %>%  # Keep only rows where event == TRUE
     mutate(year = year(t)) %>%  # Extract the year from the date
@@ -70,14 +77,87 @@ mhw_days_per_site <- lapply(names(events), function(site) {
 })
 
 # Combine results into a summary dataframe
-mhw_summary <- bind_rows(mhw_days_per_site) %>% 
+MHW_summary <- bind_rows(MHW_days_per_site) %>% 
   mutate(year = as.numeric(year))
+
+#Part 2E: Summarize marine heatwave days in each category per year for each site ####
+
+# Extract and summarize MHW days per site-category-year
+MHW_days_categorized <- lapply(names(MHW_categories), function(site) {
+  MHW_categories[[site]] %>% 
+    mutate(site_id = site,  # Add site name as a column
+           year = year(peak_date), # Extract the year from the event start date
+           #Categorize number of days in each severity category
+           days_moderate = duration * p_moderate/100, 
+           days_strong = duration * p_strong/100,
+           days_severe = duration * p_severe/100, 
+           days_extreme = duration * p_extreme/100) %>%  
+    group_by(site_id, category, year) %>%  # Group by site, category, and year
+    #Calculate sum of days in each severity category
+    summarize(days_moderate = sum(days_moderate),
+              days_strong = sum(days_strong),
+              days_severe = sum(days_severe),
+              days_extreme = sum(days_extreme),
+              .groups = "drop")  # Sum MHW days per year-category
+})
+
+names(MHW_days_categorized) <- names(events)  # Assign names to the list
+
+# Combine into a single dataframe
+MHW_categorized_summary <- bind_rows(MHW_days_categorized)
+
 
 #########################################
 # Part 3: Visualize Marine Heatwaves ####
 #########################################
 
-#coming soon! 
+#Categorized heatwave summary plot
+
+categorized_summary_plot_df <- MHW_categorized_summary %>% 
+  select(-category) %>% 
+  rename(Moderate = days_moderate, Strong = days_strong, 
+         Severe = days_severe, Extreme = days_extreme) %>% 
+  pivot_longer(cols = c("Moderate", "Strong", "Severe", "Extreme"),
+               names_to = "category", 
+               values_to = "duration") %>% 
+  group_by(site_id, category, year) %>% 
+  summarize(sum_heatwave_days = sum(duration), .groups="drop") %>% 
+  group_by(category, year) %>% 
+  summarize(mean_heatwave_days = mean(sum_heatwave_days), .groups = "drop") %>% 
+  mutate(category = factor(category, levels = c( "Extreme","Severe","Strong","Moderate")))
+
+# The MHW colour palette
+MHW_colours <- c(
+  "Moderate" = "#ffc866",
+  "Strong" = "#ff6900",
+  "Severe" = "#9e0000",
+  "Extreme" = "#2d0000"
+)
+
+
+
+categorized_summary_plot <- ggplot(categorized_summary_plot_df, aes(x=year, y=mean_heatwave_days, fill = category))+
+  geom_bar(stat="identity")+
+  scale_fill_manual(values = MHW_colours)+
+  labs(x="Year", 
+       y="Marine Heatwave Days\n(mean days across all sites)",
+       fill = "Intensity")+
+  theme_few()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1.1,hjust = 1),
+        panel.border = element_rect(linewidth = 2),
+        strip.text = element_text(face = "bold"),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"),
+        legend.position = "inside",
+        legend.position.inside = c(0.9, 0.8)) 
+categorized_summary_plot
+
+
+#Single site example plot
+
+
+
 
 ##################
 # Part 4: GAM ####
@@ -90,7 +170,7 @@ set.seed(99)
 heatwave_gam <- gam(
   mhw_days ~ s(year, k = 10) + #Year as smooth predictor
     s(site_id, bs = "re"), #Site as a random effect
-  data = mhw_summary,
+  data = MHW_summary,
   method = "REML") # Use restricted maximum likelihood for smoother estimation
 
 summary(heatwave_gam)
@@ -100,7 +180,7 @@ par(mfrow = c(2, 2)) # Set up plotting grid
 gam.check(heatwave_gam)
 
 plot(heatwave_gam, pages = 1, rug = TRUE, shade = TRUE)
-plot(residuals(heatwave_gam) ~ mhw_summary$year)
+plot(residuals(heatwave_gam) ~ MHW_summary$year)
 
 
 smooth_coefs(heatwave_gam, "s(site_id)")
@@ -127,7 +207,7 @@ gam_predictions <- data.frame(
 # Part 4: Heatwave Summary Plot ####
 ####################################
 
-heatwave_summary_plot <- ggplot(mhw_summary, aes(x=year)) +
+heatwave_summary_plot <- ggplot(MHW_summary, aes(x=year)) +
   geom_point(aes(y=mhw_days),color = "red")+
   geom_line(data = gam_predictions, aes(y=fit), 
             color = "red", linewidth = 1)+
