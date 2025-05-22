9a8c0189703f115f26ecdb086f638996a2224bf9
Frankie Gerraty <frankiegerraty@gmail.com>
2025-05-22 16:25:16 -0700

    working on mussel data

--- scripts/05_Mussels.R
+++ scripts/05_Mussels.R
@@ -23,9 +23,18 @@ mussels <- read_csv("data/processed/mussels.csv") %>%
   #Assign period based on SSWD status
   mutate(period = factor(period, levels = c("pre_ssw", "post_ssw")))
 
-bio_surveys <- mussels %>% 
+cbs_surveys <- mussels %>% 
   group_by(site_id, period) %>% 
   summarise(survey_years = length(unique(year)), .groups = "drop")
+
+mussel_percent_cover <- read_csv("data/processed/mussel_percent_cover.csv") %>% 
+  #Assign period = pre- or post-SSWD
+  mutate(period = if_else(year < 2014, "pre_ssw", "post_ssw")) %>% 
+  #Remove data from sites without surveys in pre- and post-SSWD periods
+  filter(!site_id %in% c(2, 5, 11)) %>% 
+  #Assign period based on SSWD status
+  mutate(period = factor(period, levels = c("pre_ssw", "post_ssw")),
+         site_id = factor(site_id))
 
 
 #####################################################
@@ -35,7 +44,7 @@ bio_surveys <- mussels %>%
 heatmap_df <- mussels %>% 
   group_by(site_id, period, transect, location) %>% 
   summarise(mussel_years = n(), .groups = "drop") %>% 
-  right_join(bio_surveys, by = join_by(site_id, period)) %>% 
+  right_join(cbs_surveys, by = join_by(site_id, period)) %>% 
   mutate(mussel_freq = mussel_years/survey_years) %>% 
   select(-mussel_years, -survey_years) %>% 
   pivot_wider(names_from = period, values_from = mussel_freq, 
@@ -113,39 +122,40 @@ ggsave("output/extra_figures/mussel_heatmap_example_site.png", heatmap_example,
        width = 8, height = 5, units = "in", dpi = 600)
 
 
-##########################################
-# Part 3: Mussel Depth Distribution  #####
-##########################################
+###############################################
+# Part 3: Mussel Depth Distribution Plot  #####
+###############################################
 
-mussel_depth_distribution <- ggplot(mussels, aes(x=tidal_elevation, fill = period, color = period))+
+mussel_tidal_height_distribution <- ggplot(mussels, aes(x=tidal_elevation, fill = period, color = period))+
   geom_density(alpha = .8)+
-  geom_boxplot(data = mussels %>% filter(period == "pre_ssw"), 
+  geom_boxplot(data = mussels %>% filter(period == "post_ssw"), 
                aes(x = as.numeric(tidal_elevation), y = .78),   
                width = 0.04, alpha = 0.7, outlier.shape = NA) +
-  geom_boxplot(data = mussels %>% filter(period == "post_ssw"), 
+  geom_boxplot(data = mussels %>% filter(period == "pre_ssw"), 
                aes(x = as.numeric(tidal_elevation), y = .85),   
                width = 0.04, alpha = 0.7, outlier.shape = NA) +
   scale_x_reverse()+
   scale_color_manual(values = c("#d8be02", "#0088d9"), labels = c("Pre-SSWD", "Post-SSWD"))+
   scale_fill_manual(values = c("#d8be02", "#0088d9"), labels = c("Pre-SSWD", "Post-SSWD"))+
-  labs(y="Frequency", x="Mussel depth\n(meters from MLLW)", 
+  labs(y="Frequency", x="Mussel tidal height\n(meters from MLLW)", 
        fill = "Period", color = "Period")+
   theme_few()+
   theme(panel.border = element_rect(linewidth = 1.2),
         axis.title.x = element_text(face = "bold"),
         axis.title.y = element_text(face = "bold"),
         legend.title = element_text(face = "bold"),
-        legend.position = "none")
-mussel_depth_distribution
+        legend.position = "none"
+        )
+mussel_tidal_height_distribution
 
 
-ggsave("output/extra_figures/mussel_depth_distribution.png", mussel_depth_distribution, 
-       width = 3.5, height = 2.75, units = "in", dpi = 600)
+ggsave("output/extra_figures/mussel_tidal_height_distribution.png", mussel_tidal_height_distribution, 
+       width = 4, height = 3, units = "in", dpi = 600)
 
 
-###################################################
-# Part 3: Mussel Depth Distribution Analysis  #####
-###################################################
+######################################################
+# Mussel Depth Distribution Analysis (WORKING!)  #####
+######################################################
 
 model_df <- mussels %>% 
   mutate(tidal_elevation_shifted = tidal_elevation+2.301)
@@ -165,4 +175,48 @@ testDispersion(model_sim)
 plot(model)
 
 
+##########################################
+# Part 3: Mussel Percent Cover Plot  #####
+##########################################
 
+mussel_percent_cover_plot <- ggplot(mussel_percent_cover, aes(x=percent_cover, fill = period, color = period))+
+  geom_density(alpha = .8)+
+  geom_boxplot(data = mussel_percent_cover %>% filter(period == "pre_ssw"), 
+               aes(x = percent_cover, y = .03),   
+               width = 0.0015, alpha = 0.7, outlier.shape = NA) +
+  geom_boxplot(data = mussel_percent_cover %>% filter(period == "post_ssw"), 
+               aes(x = percent_cover, y = .0273),   
+               width = 0.0015, alpha = 0.7, outlier.shape = NA) +
+  scale_color_manual(values = c("#d8be02", "#0088d9"), labels = c("Pre-SSWD", "Post-SSWD"))+
+  scale_fill_manual(values = c("#d8be02", "#0088d9"), labels = c("Pre-SSWD", "Post-SSWD"))+
+  labs(y="Frequency", x="Percent Cover", 
+       fill = "Period", color = "Period")+
+  theme_few()+
+  theme(panel.border = element_rect(linewidth = 1.2),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"),
+        legend.position = "none"
+        )
+mussel_percent_cover_plot
+
+ggsave("output/extra_figures/mussel_percent_cover.png", 
+       mussel_percent_cover_plot, 
+       width = 4, height = 2.8, units = "in", dpi = 600)
+
+
+
+
+
+pc_model <- glmmTMB(percent_cover/100 ~ period + 
+                   (1 | site_id), 
+                 data = mussel_percent_cover,
+                 family=beta_family(link="logit"))
+summary(pc_model)
+
+
+# Check h1d assumptions with DHARMa package
+pc_res = simulateResiduals(pc_model)
+plot(pc_res, rank = T)
+testDispersion(pc_res)
+plotResiduals(pc_res, mussel_percent_cover$site_id, xlab = "Site", main=NULL)
